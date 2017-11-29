97ee88cfe2400a6cad401b7b6c1512d1832e5935
nschool <nschool@amazon.com>
2017-11-28 20:57:15 -0800

    Working on porting to Arduino

--- moveglow.ino
+++ moveglow.ino
@@ -1,42 +1,55 @@
+#include <I2Cdev.h>
+#include <FastLED.h>
+#include <Wire.h>
+#include <math.h>
+#define PI 3.14159265
+#define NUM_LEDS 144
+#define DATA_PIN 6
 
-frequency = 100; // Set reading frequency [Hz] - readings per second.
+int offsets[] = {-1524,-89,1450,-9,-32,25};
 
-multiplier = 9.81; // Set multiplier to convert accelerometer data to acceleration m/s^2
-acc_threshold = 0.2; // Set threshold to ignore small noise in acceleration
-xAcc_Filtered = 0; // initialise filtered value to 0
-acceleration_Filtered=0;
-velocity_Filtered =0;
+double accX, accY, accZ;
+
+
+
+int frequency = 100; // Set reading frequency [Hz] - readings per second.
+
+double multiplier = 9.81; // Set multiplier to convert accelerometer data to acceleration m/s^2
+double acc_threshold = 0.2; // Set threshold to ignore small noise in acceleration
+int xAcc_Filtered = 0; // initialise filtered value to 0
+int acceleration_Filtered=0;
+int velocity_Filtered =0;
 
 
 // coefficient to apply filtering, adjust these to give better result
-alpha = 0.15;
-beta = 0.95;
-gamma = 0.9;
+double alpha = 0.15;
+double beta = 0.95;
+double gamma = 0.9;
 
 
 // Initialise variables for calculation
-current_time = now;
-t0 = now;
-previous_time =0;
-previous_velocity =0;
-current_velocity = 0;
-total_displacement = 0;
-
-
-if (~exist('e', 'var'))
- e = engduino();
-end
+long current_time = millis();
+long t0 = millis();
+long previous_time = 0;
+int previous_velocity =0;
+int current_velocity = 0;
+int total_displacement = 0;
 
 
 
 void Setup() {
 
+  Serial.begin(115200);
+  initFastLED();
+  initMPUraw();
+  
 //// Initialise accelerometer reading
-for i=1:5
- newReading = e.getAccelerometer();
- gx = newReading(1);
+for {i=0; i < 5; i++){
+ getData()
+ gx = accZ;
  xAcc_Filtered = gx - ((1-alpha)*xAcc_Filtered + alpha*gx); // apply high pass filter to the accelerometer output
-end
+}
+
 init_accx = (floor(xAcc_Filtered*100)*multiplier/100); // accelerometer data is multiplied to get 1g=10m/s^2
 
 }
@@ -45,26 +58,29 @@ init_accx = (floor(xAcc_Filtered*100)*multiplier/100); // accelerometer data is 
 
 void Loop() {
 
-current_time = (now - t0)*10e4; // Record the current time
-newReading = e.getAccelerometer();
-gx = newReading(1);
+current_time = millis() - t0;
+
+getData();
+
+gx = accZ;
 xAcc_Filtered = gx - ((1-alpha)*xAcc_Filtered + alpha*gx); // apply high pass filter to the accelerometer output
 acceleration = (floor(xAcc_Filtered*100)*multiplier/100 - init_accx); // convert to acceleration from accelerometer
 
 accFilt = (1-beta)*accFilt + beta*acceleration;
 // ignore small value acceleration due to noise
-if(accFilt>-acc_threshold&&accFilt<acc_threshold)
+if( accFilt > -acc_threshold && accFilt < acc_threshold){
  accFilt = 0;
-end
+}
 
-x=sym(acceleration_Filtered);
+x=accFilt;
 
 // Calculate velocity
-current_velocity = previous_velocity + int(x, previous_time,
-current_time);
+current_velocity = previous_velocity + int(x, previous_time, current_time);
 // low pass filter to filter out noise from calculated velocity
 velocity_Filtered = (1-gamma)*velocity_Filtered + gamma*current_velocity;
 // Integrate velocity to displacement
+
+
 displacement = int(current_velocity, previous_time, current_time);
 total_displacement = total_displacement + displacement;
 previous_velocity = velocity_Filtered;
@@ -78,6 +94,16 @@ pause(1/frequency);
 }
 
 
+
+void getData() {
+  uint32_t  timer2 = micros();
+  while (i2cRead(0x3B, i2cData, 14)) {
+    if (timer2 > 5000) {break;}
+  }
+  accX = ((i2cData[0] << 8) | i2cData[1]);
+  accY = ((i2cData[2] << 8) | i2cData[3]);
+  accZ = ((i2cData[4] << 8) | i2cData[5]);
+}
 
 
 
